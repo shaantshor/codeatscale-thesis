b4eb8381b2c37772d00e4d13da738aac4ade3cbe
shaantshor <sakshipalve08@gmail.com>
2026-06-28 23:29:21 +0100

    fixing names

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/codeatscale/',
+  base: '/codeatscale-thesis/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['pyodide'],
